1cabf93e2109cee2a6d9b4be7dfd03959df717d0
Melvyn Gomez <melvyng@iadb.org>
2026-02-19 16:49:13 -0600

    Fix Missing Standard File Headers Closes #5

--- db/access.php
+++ db/access.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,15 +12,15 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Capability definitions for Plugin Replacer.
  *
  * @package    local_pluginreplace
- * @copyright  2026 OpenRanger S. A. de C.V. (https://openranger.com/)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author     Melvyn Gomez - OpenRanger (melvyng@openranger.com)
+ * @copyright  2025 Melvyn Gomez (https://openranger.com/)
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();

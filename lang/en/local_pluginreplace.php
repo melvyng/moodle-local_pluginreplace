ece6693e391ef5638c235e42c04e13a0f50d2907
Melvyn Gomez <melvyng@iadb.org>
2026-02-19 16:10:13 -0600

    Add missing capability language string
    Closes #6

--- lang/en/local_pluginreplace.php
+++ lang/en/local_pluginreplace.php
@@ -1,5 +1,5 @@
 <?php
-// This file is part of Moodle - http://moodle.org/
+// This file is part of Moodle - https://moodle.org/
 //
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
@@ -12,22 +12,24 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
 
 /**
  * Strings for component 'local_pluginreplace', language 'en'.
  *
  * @package    local_pluginreplace
- * @copyright  2026 OpenRanger S. A. de C.V. (https://openranger.com/)
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  * @author     Melvyn Gomez - OpenRanger (melvyng@openranger.com)
+ * @copyright  2025 Melvyn Gomez (https://openranger.com/)
+ * @license    https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
 
 $string['diff'] = 'File differences';
 $string['invalidplugin'] = 'Invalid or unknown plugin';
 $string['notinstalled'] = 'Plugin is not installed';
 $string['pluginname'] = 'Plugin replacer';
-
+$string['pluginreplace:replace'] = 'Replace plugins';
 $string['preview'] = 'Preview changes';
 $string['replace'] = 'Replace plugin';
 $string['success'] = 'Plugin replaced successfully. Redirecting to upgrade…';
